9b014ccb21ec100be8f9b4242e87468f1638a4dd
conflabermits <christopher.dunaj@gmail.com>
2025-03-23 14:40:53 -0400

    Fix handlers for named paths by adding to custom mux

--- main.go
+++ main.go
@@ -12,9 +12,12 @@ func main() {
 	port := flag.String("port", "8080", "Port to run the local web server")
 	flag.Parse()
 
-	http.Handle("/ok", httpfunctions.HTTPMiddleware(http.HandlerFunc(httpfunctions.Respond_ok)))
-	http.Handle("/degraded", httpfunctions.HTTPMiddleware(http.HandlerFunc(httpfunctions.Respond_degraded)))
-	http.Handle("/outage", httpfunctions.HTTPMiddleware(http.HandlerFunc(httpfunctions.Respond_outage)))
+	mux := http.NewServeMux()
+	httpfunctions.RegisterStatusCodeHandlers(mux)
+
+	mux.Handle("/ok", httpfunctions.HTTPMiddleware(http.HandlerFunc(httpfunctions.Respond_ok)))
+	mux.Handle("/degraded", httpfunctions.HTTPMiddleware(http.HandlerFunc(httpfunctions.Respond_degraded)))
+	mux.Handle("/outage", httpfunctions.HTTPMiddleware(http.HandlerFunc(httpfunctions.Respond_outage)))
 	/*
 		http.Handle("/200", httpfunctions.HTTPMiddleware(http.HandlerFunc(httpfunctions.Respond_200)))
 		http.Handle("/301", httpfunctions.HTTPMiddleware(http.HandlerFunc(httpfunctions.Respond_301)))
@@ -23,10 +26,7 @@ func main() {
 		http.Handle("/404", httpfunctions.HTTPMiddleware(http.HandlerFunc(httpfunctions.Respond_404)))
 		http.Handle("/500", httpfunctions.HTTPMiddleware(http.HandlerFunc(httpfunctions.Respond_500)))
 	*/
-	http.Handle("/headers", httpfunctions.HTTPMiddleware(http.HandlerFunc(httpfunctions.Respond_headers)))
-
-	mux := http.NewServeMux()
-	httpfunctions.RegisterStatusCodeHandlers(mux)
+	mux.Handle("/headers", httpfunctions.HTTPMiddleware(http.HandlerFunc(httpfunctions.Respond_headers)))
 
 	log.Printf("Server starting on http://localhost:%s\n", *port)
 	if err := http.ListenAndServe(":"+*port, mux); err != nil {
